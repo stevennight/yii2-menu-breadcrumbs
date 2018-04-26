1a7e02e6171b268f86e79c476cf258e385974596
StevenNight <stevennight@qq.com>
2018-04-26 17:31:06 +0800

    Init the project.

---
+++ src/MenuBreadcrumbs.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stevennight
+ * Date: 2018/4/26
+ * Time: 17:29
+ */
+
+namespace stevennight\menu_breadcrumbs;
+
+use dmstr\widgets\Menu;
+
+class MenuBreadcrumbs extends Menu
+{
+
+}
